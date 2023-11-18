1206519abfd1e18f0d586788e8a29f7d2546b606
Casper Engelmann <c.engelmann@custimy.io>
2023-11-18 02:04:12 +0100

    Add frontpage.blade.php and remove welcome.blade.php

---
+++ resources/views/frontpage.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.site')
+
+@section('title')
+Frontpage
+@endsection
+
+@section('content')
+<x-frontpage-hero class="mt-12"></x-frontpage-hero>
+@endsection
--- resources/views/welcome.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.site')
-
-@section('content')
-<h1>MBX</h1>
-<p>MBX is a simple, lightweight, and fast MVC framework for PHP.</p>
-@endsection
