90ef4eae5a5cc64d43730b7ae4c226688a234b4c
Casper Engelmann <c.engelmann@custimy.io>
2023-11-18 02:04:05 +0100

    Add frontpage hero component view

---
+++ resources/views/components/frontpage-hero.blade.php
@@ -0,0 +1,31 @@
+<div
+    {{ $attributes->merge([
+        'class' => 'container lg:flex-row flex flex-col-reverse items-center p-4',
+    ]) }}>
+    <div class="lg:w-2/5 lg:space-y-10 w-full px-4 space-y-4">
+        <div class="lg:items-start flex flex-col items-center space-y-4 leading-tight">
+            <h1 class="text-3xl lg:text-left w-full text-center">Regeringen arbejder for europæisk regulering mod unges
+                onlinegambling</h1>
+            <p class="md:text-xl text-lg lg:text-left block w-full max-w-md text-center">
+                {{ widont('Skatteminister Jeppe Bruus (S) søger europæisk regulering for at bekæmpe onlinegambling blandt børn og unge under 18 år.') }}
+            </p>
+        </div>
+        <div class="pt-2 lg:pt-0 lg:text-left text-center">
+            <a href="#"
+                class="group inline-flex items-center gap-1.5 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <span>Læs mere</span>
+                <svg class="w-4 h-4 transform group-hover:translate-x-0.5 transition" focusable="false" viewBox="0 0 12 13"
+                    fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M9.1275 6.75H0.5V6.25H9.1275H10.3331L9.48137 5.39676L5.64929 1.55782L6 1.20711L11.2929 6.5L6 11.7929L5.64929 11.4422L9.48137 7.60324L10.3331 6.75H9.1275Z">
+                    </path>
+                </svg>
+            </a>
+        </div>
+    </div>
+    <div class="lg:mb-0 lg:w-3/5 w-full px-4 mb-4">
+        <div class="flex w-full">
+            <img src="/poker.webp" alt="" class="object-cover w-full rounded-lg" width="736" height="428">
+        </div>
+    </div>
+</div>
